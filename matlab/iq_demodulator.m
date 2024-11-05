cfef1169b004d780491abb2fea603d181615a944
mstoyanovca <mstoyanovca@gmail.com>
2024-11-04 20:10:31 -0500

    added iq demodulator

--- matlab/iq_demodulator.m
+++ matlab/iq_demodulator.m
@@ -7,69 +7,74 @@ clc
 
 fs = 65e6
 t = 0 : 1/fs : 1e-3 - 1/fs
-
-fa = 1e3
-audio = sin(2*pi*fa*t)
-fc = 7.074e6
-usb = ssbmod(audio, fc, fs, 0, "upper")
 df = -fs/2 : 1e3 : fs/2-1e3
 
-dds_sin = sin(2*pi*fc*t)
-dds_cos = cos(2*pi*fc*t)
+fc = 7.074e6
+fa = 1e3
+audio = sin(2*pi*fa*t)
+usb = ssbmod(audio, fc, fs, 0, "upper")
 
-i = dds_sin.*usb
-q = dds_cos.*usb
+dds_i = cos(2*pi*fc*t)
+dds_q = sin(2*pi*fc*t)
 
+i = usb.*dds_i
+q = usb.*dds_q
 
 figure(1)
-subplot(5, 1, 1)
+subplot(4, 1, 1)
 plot(df, abs(fftshift(fft(usb)/length(df))))
-axis([-10e6, 10e6, 0, 1])
+hold on
+hold off
+axis([-10e6, 10e6, 0, 0.6])
 title('USB')
 xlabel('Hz')
 ylabel('A')
 grid on
-subplot(5, 1, 2)
-plot(t, dds_sin)
+subplot(4, 1, 2)
+plot(t, dds_i)
 hold on
-plot(t, dds_cos)
+plot(t, dds_q)
 hold off
 axis([0, 1e-6, -1, 1])
 title('DDS')
 xlabel('s')
 ylabel('A')
+legend('DDS I', 'DDS Q')
 grid on
-subplot(5, 1, 3)
-plot(t, i)
-hold on
-plot(t, q)
-hold off
-axis([0, 1e-3, -1, 1])
-title('I and Q at 65MSPS')
-xlabel('s')
-ylabel('A')
-grid on
-subplot(5, 1, 4)
+subplot(4, 1, 3)
 {
   # Matlab doesn't allow a decimation factor above 13 in a single step:
-  dec_factor = 13*13*9
+  dec_factor = 13*5*5*5
   fs = fs/dec_factor
-  t = 0 : 1/fs : 1e-3
+  t = 0 : 1/fs : 1e-3 - 1/fs
+  df = -fs/2 : 1e3 : fs/2-1e3
 
-  i1 = decimate(i, 13)
-  q1 = decimate(q, 13)
-  i2 = decimate(i1, 13)
-  q2 = decimate(q1, 13)
-  i = decimate(i2, 9)
-  q = decimate(q2, 9)
+  temp_i = i
+  temp_q = q
+  i = decimate(decimate(decimate(decimate(temp_i, 13), 5), 5), 5)
+  q = decimate(decimate(decimate(decimate(temp_q, 13), 5), 5), 5)
 }
 stem(t, i)
 hold on
 stem(t, q)
 hold off
-axis([0, 1e-3, -1, 1])
-title('I and Q at 44KSPS')
+axis([0, 1e-3, -0.6, 0.6])
+title('IQ at 40KSPS')
 xlabel('s')
 ylabel('A')
+legend('I', 'Q')
+grid on
+subplot(4, 1, 4)
+plot(df, abs(fftshift(fft(i))/length(df)))
+hold on
+plot(df, abs(fftshift(fft(q)/length(df))))
+hold on
+plot(df, abs(fftshift(fft(i+q)/length(df))))
+hold off
+axis([-3e3, 3e3, 0, 0.5])
+title('audio')
+xlabel('Hz')
+ylabel('A')
+legend('I', 'Q', 'I+Q')
 grid on
 
---
+++ matlab/iq_modulator.m
@@ -0,0 +1,64 @@
+addpath("C:\Users\Martin Stoyanov\IdeaProjects\va3aui\matlab")
+pkg load communications
+pkg load signal
+clear all
+close all
+clc
+
+fs = 65e6
+t = 0 : 1/fs : 1e-3 - 1/fs
+df = -fs/2 : 1e3 : fs/2-1e3
+
+fa = 1e3
+audio = sin(2*pi*fa*t)
+audio_i = -imag(hilbert(audio))
+audio_q = audio
+
+fc = 7.074e6
+dds_i = cos(2*pi*fc*t)
+dds_q = sin(2*pi*fc*t)
+
+i = audio_i.*dds_i
+q = audio_q.*dds_q
+
+figure(1)
+subplot(3, 1, 1)
+plot(t, audio_i)
+hold on
+plot(t, audio_q)
+hold off
+axis([0, 1e-3, -1, 1])
+title('Audio')
+xlabel('s')
+ylabel('A')
+legend('Audio I', 'Audio Q')
+grid on
+subplot(3, 1, 2)
+plot(t, dds_i)
+hold on
+plot(t, dds_q)
+hold off
+axis([0, 1e-6, -1, 1])
+title('DDS')
+xlabel('s')
+ylabel('A')
+legend('DDS I', 'DDS Q')
+grid on
+subplot(3, 1, 3)
+plot(df, abs(fftshift(fft(i)/length(df))))
+hold on
+plot(df, abs(fftshift(fft(q)/length(df))))
+hold on
+# LSB:
+plot(df, abs(fftshift(fft(i+q)/length(df))))
+hold on
+# USB:
+plot(df, abs(fftshift(fft(i-q)/length(df))))
+hold off
+axis([-10e6, 10e6, 0, 0.6])
+title('SSB')
+xlabel('Hz')
+ylabel('A')
+legend('I', 'Q', 'LSB', 'USB')
+grid on
+
