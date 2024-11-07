36fb2d6f19d2070de7e344482e6188422c5b76d0
mstoyanovca <mstoyanovca@gmail.com>
2024-11-06 20:37:50 -0500

    added sdr vhdl

---
+++ vhdl/adc/adc.gen/sources_1/ip/ila_0/ila_0_stub.v
@@ -0,0 +1,26 @@
+// Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+// Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2024.1 (win64) Build 5076996 Wed May 22 18:37:14 MDT 2024
+// Date        : Wed Nov 20 18:46:59 2024
+// Host        : DESKTOP running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub {c:/Users/Martin
+//               Stoyanov/IdeaProjects/va3aui/vhdl/adc/adc.gen/sources_1/ip/ila_0/ila_0_stub.v}
+// Design      : ila_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg484-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* X_CORE_INFO = "ila,Vivado 2024.1" *)
+module ila_0(clk, probe0, probe1, probe2, probe3)
+/* synthesis syn_black_box black_box_pad_pin="probe0[0:0],probe1[9:0],probe2[0:0],probe3[9:0]" */
+/* synthesis syn_force_seq_prim="clk" */;
+  input clk /* synthesis syn_isclock = 1 */;
+  input [0:0]probe0;
+  input [9:0]probe1;
+  input [0:0]probe2;
+  input [9:0]probe3;
+endmodule
---
+++ vhdl/adc/adc.ip_user_files/sim_scripts/activehdl/compile.do
@@ -0,0 +1,27 @@
+transcript off
+onbreak {quit -force}
+onerror {quit -force}
+transcript on
+
+vlib work
+vlib activehdl/xpm
+vlib activehdl/xil_defaultlib
+
+vmap xpm activehdl/xpm
+vmap xil_defaultlib activehdl/xil_defaultlib
+
+vlog -work xpm  -sv2k12 "+incdir+../../../adc.gen/sources_1/ip/clk_wiz_0" -l xpm -l xil_defaultlib \
+"D:/Xilinx/Vivado/2024.1/data/ip/xpm/xpm_cdc/hdl/xpm_cdc.sv" \
+"D:/Xilinx/Vivado/2024.1/data/ip/xpm/xpm_memory/hdl/xpm_memory.sv" \
+
+vcom -work xpm -93  \
+"D:/Xilinx/Vivado/2024.1/data/ip/xpm/xpm_VCOMP.vhd" \
+
+vlog -work xil_defaultlib  -v2k5 "+incdir+../../../adc.gen/sources_1/ip/clk_wiz_0" -l xpm -l xil_defaultlib \
+"../../../adc.gen/sources_1/ip/ila_0/sim/ila_0.v" \
+"../../../adc.gen/sources_1/ip/clk_wiz_0/clk_wiz_0_clk_wiz.v" \
+"../../../adc.gen/sources_1/ip/clk_wiz_0/clk_wiz_0.v" \
+
+vlog -work xil_defaultlib \
+"glbl.v"
+
