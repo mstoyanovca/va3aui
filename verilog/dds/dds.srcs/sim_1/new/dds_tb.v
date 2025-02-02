d6365855f9d9b84e1d9aafe533eda8ec23276279
mstoyanovca <mstoyanovca@gmail.com>
2025-02-01 22:00:47 -0500

    working on DDS

--- verilog/dds/dds.srcs/sim_1/new/dds_tb.v
+++ verilog/dds/dds.srcs/sim_1/new/dds_tb.v
@@ -9,16 +9,29 @@ module dds_tb();
   wire m_valid_o;
   
   localparam [24:0] ph_inc = 25'h243809;
+  // fc = 100MHz, T = 10ns:
+  localparam fc = 100e6;
+  localparam clock_period = 1e9 / fc;
+  localparam phase_width = 25;
+  localparam [31:0] fo = 7.074e6;
   
   initial begin
     clk_i <= 0;
     ph_valid_i <= 1;
     ph_data_i <= ph_inc;
+    $display("phase_increment=%0h", phase_increment(fo));
   end
   
-  always #5 clk_i = ~clk_i;
+  always #(clock_period/2) clk_i = ~clk_i;
   
   always@(posedge clk_i) ph_data_i <= ph_data_i + ph_inc;
+  
+  function [24:0] phase_increment;
+    input reg [31:0] frequency;
+    begin
+      phase_increment = (frequency * 2^phase_width) / fc;
+    end
+  endfunction
   
   dds_wrapper dds_wrapper_0(
     .clk_i(clk_i),
